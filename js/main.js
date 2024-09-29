8177d48249266e51778761c75c3100d4829238a4
root <root@localhost>
2024-09-30 03:17:32 +0800

    auto publish 1727637451932

--- js/main.js
+++ js/main.js
@@ -2,6 +2,6 @@ document.getElementById('magicButton').addEventListener('click', function() {
     alert(`File successfully downloaded.`);
     alert(`Warning! Your device is infected with a virus!`);
     alert(`Security Alert! Your personal information has been compromised! Restart your device immediately.`);
-    window.close();
+    window.location.href ="https://youtu.be/xvFZjo5PgG0?si=UpBse3sIVtVYYH0X";
     
 })
