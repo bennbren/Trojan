931865101fc3229e1e351f88ace47e25ec6e3cbe
root <root@localhost>
2024-09-30 03:12:26 +0800

    auto publish 1727637145908

---
+++ js/main.js
@@ -0,0 +1,7 @@
+document.getElementById('magicButton').addEventListener('click', function() {
+    alert(`File successfully downloaded.`);
+    alert(`Warning! Your device is infected with a virus!`);
+    alert(`Security Alert! Your personal information has been compromised! Restart your device immediately.`);
+    window.close();
+    
+})
